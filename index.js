169d3a135b43f886412f7e923b7cd8377862cc5d
FAStstorebitbankuk <faststorebit@outlook.com>
2024-11-27 09:52:37 -0800

    add changes in index.js

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ const ApiServices = async () => {
     const client = new CovalentClient("cqt_rQtw8GqQ7cfXHBjXPtT7V6qG4r8J");
     const resp = await client.BalanceService.getTokenBalancesForWalletAddress("eth-mainnet", "demo.eth");
     console.log(resp.data);
-}
+};
+// Call the ApiServices function to execute the API call
+ApiServices();
