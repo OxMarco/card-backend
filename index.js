2660cbfb5d27c5f3b418d2361183c5cd9b38ebb1
FAStstorebitbankuk <faststorebit@outlook.com>
2024-11-27 09:46:43 -0800

    add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import { CovalentClient } from "@covalenthq/client-sdk";
+
+// Your code here
+const ApiServices = async () => {
+    const client = new CovalentClient("cqt_rQtw8GqQ7cfXHBjXPtT7V6qG4r8J");
+    const resp = await client.BalanceService.getTokenBalancesForWalletAddress("eth-mainnet", "demo.eth");
+    console.log(resp.data);
+}
