4f11a2564f480550d3a26824d1d852399261af50
FAStstorebitbankuk <faststorebit@outlook.com>
2024-11-27 10:20:59 -0800

    add changes

---
+++ backend/src/apiService.ts
@@ -0,0 +1,13 @@
+import { Client } from "@covalenthq/client-sdk";
+
+const ApiServices = async () => {
+    const client = new Client("cqt_rQtw8GqQ7cfXHBjXPtT7V6qG4r8J");
+    const resp = await client.BalancesService.getTokenBalancesForWalletAddress({
+        chainId: "eth-mainnet",
+        address: "demo.eth"
+    });
+    console.log(resp.data);
+};
+
+// Call the function to execute the API call
+ApiServices();
